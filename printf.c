d14d0fcc6c968b0a8f5c34972d11e9e64ab73b51
BIG-FAN <shehan.DA619@yahoo.com>
2015-02-24 23:37:35 +0530

    messing with pointer + bitmasking
    just tried something to see if it works and well it works :D

--- printf.c
+++ printf.c
@@ -2,8 +2,15 @@
 
 int main(void){
 
-  char note[] = hello;
+  char note[] = "hello";
+  char *ptr = note;
+  
   printf("test %s",note)
+  
+  ptr = ptr++;
+  *ptr = *ptr ^ 0x20;
+  
+  printf("\n\n%s",note);
 
   return 0;
 }
